ed20524927b07d6c74da29f25cd79d2dd6f1dae1
FullGreen.GN <61293366+FullGreenGN@users.noreply.github.com>
2024-08-03 16:09:46 +0200

    get nums

--- ex00/src/main.c
+++ ex00/src/main.c
@@ -6,7 +6,7 @@
 /*   By: fullgreen <fullgreen@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 14:43:25 by fullgreen         #+#    #+#             */
-/*   Updated: 2024/08/03 15:57:55 by fullgreen        ###   ########.fr       */
+/*   Updated: 2024/08/03 16:09:33 by fullgreen        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,36 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+int	getdec(int nb)
+{
+	if (nb >= 90)
+		return (90);
+	else if (nb >= 80)
+		return (80);
+	else if (nb >= 70)
+		return (70);
+	else if (nb >= 60)
+		return (60);
+	else if (nb >= 50)
+		return (50);
+	else if (nb >= 40)
+		return (40);
+	else if (nb >= 30)
+		return (30);
+	else if (nb >= 20)
+		return (20);
+	else if (nb <= 20)
+		return (nb);
+	else
+		return (0);
+}
+
+int	getmult(int nb)
+{
+	if (nb >= 100)
+		return (100);
+	return (getdec(nb));
+}
 
 int	main(int ac, char **av)
 {
