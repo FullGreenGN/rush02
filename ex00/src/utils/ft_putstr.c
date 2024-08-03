03ca8255e1d9eb042138ca2115aca66cedab0eda
FullGreen.GN <61293366+FullGreenGN@users.noreply.github.com>
2024-08-03 14:59:58 +0200

    change main folder

--- ex00/src/utils/ft_putstr.c
+++ ex00/src/utils/ft_putstr.c
@@ -6,19 +6,13 @@
 /*   By: fullgreen <fullgreen@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 14:43:24 by fullgreen         #+#    #+#             */
-/*   Updated: 2024/08/03 14:43:44 by fullgreen        ###   ########.fr       */
+/*   Updated: 2024/08/03 14:50:35 by fullgreen        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char    ft_putstr(char *str)
 {
-    int i;
-
-    i = 0;
-    while (str[i] != '\0')
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
+    while (*str)
+        write(1, str++, 1);
     return (0);
 }
