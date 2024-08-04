998e527f2fd5f76115eb9a79fb6abbf817aeef66
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-08-04 14:03:23 +0200

    renormed again

--- ex00/src/utils/ft_readfile.c
+++ ex00/src/utils/ft_readfile.c
@@ -6,15 +6,15 @@ t_list    ft_open()
 {
     int fd;
     char    buffer[1024];
+    ssize_t bytesRead;
+
 
     fd = open("numbers.dict", O_RDONLY);
-    if (fd == -1) {
+    if (fd == -1)
+    {
         ft_putstr("Error\n");
         return;
     }
-    
-    ssize_t bytesRead;
-
     while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0)
         write(1, buffer, bytesRead);
     close(fd);
