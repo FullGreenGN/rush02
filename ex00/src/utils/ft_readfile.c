b19ca3f9a1b4273d002cdeb74369a34a5202a95d
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-08-04 13:59:54 +0200

    renormed ft_readfile

--- ex00/src/utils/ft_readfile.c
+++ ex00/src/utils/ft_readfile.c
@@ -2,24 +2,20 @@
 #include <fcntl.h>
 #include "../../includes/ft.h"
 
-void ft_open() {
-    int fd = open("numbers.dict", O_RDONLY);
+t_list    ft_open() 
+{
+    int fd;
+    char    buffer[1024];
+
+    fd = open("numbers.dict", O_RDONLY);
     if (fd == -1) {
-        ft_putstr("Erreur lors de l'ouverture du fichier.\n");
+        ft_putstr("Error\n");
         return;
     }
-
-    char buffer[1024];
+    
     ssize_t bytesRead;
 
-    while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0) {
+    while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0)
         write(1, buffer, bytesRead);
-    }
-
     close(fd);
-}
-
-int main() {
-    ft_open();
-    return 0;
 }
